b8b3810e507ad692e938023b4fd43a7e366eba6e
JesusFranC <jesusfranciscocerda@gmail.com>
2023-03-16 17:44:27 -0700

    Update DataToExcel.py

--- DataToExcel.py
+++ DataToExcel.py
@@ -3,9 +3,36 @@ import os
 from openpyxl import *
 import datetime
 
-#first, we initialize and make sure that the file exists
+#first, we initialize and make sure that the Excel file exists
     #We create if not existing, and we return if it does
-    #The file name is Delivery_Stats_{Year Name}
-#We then 
+    #The filename is "Delivery_Stats_{Year}.XLSX"
+#Second, We initialize and make sure that a JSON file exists
+    #We create it if it does not exist, then return it once its there
+    #The filename is "Delivery_Stats_{Year}.JSON"
 
-#We then start a gui asking whether we 
+#We then start a gui asking what we would like to do
+    #(Current options would only be to add, remove or edit a trip)
+    #all functions will be added, and all changes will be made to the JSON String.
+    #The JSON is constantly saved, after each and every option is finished running
+
+#
+
+#All changes are finished as soon as we are done running commands at the end of the program.
+#Before the program finsishes running, it will lastly wipe and recreate the XLSX File, as a new updated version
+    #It does this using the information from the JSON File, Reading and updating all row by row
+    #All info will be sorted by date
+
+"""
+Functions needed:
+VerifyJSON:
+    
+VerifyXL:
+    
+addTrip:
+
+deleteTrip:
+
+editTrip:
+
+recreateJSON:
+"""
