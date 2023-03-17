94b00f64255aec950f4738a022476abff18bb2af
JesusFranC <94206855+JesusFranC@users.noreply.github.com>
2023-03-16 17:29:29 -0700

    Update DataToExcel.py

---
+++ DataToExcel.py
@@ -0,0 +1,11 @@
+#This will be the main
+import os
+from openpyxl import *
+import datetime
+
+#first, we initialize and make sure that the file exists
+    #We create if not existing, and we return if it does
+    #The file name is Delivery_Stats_{Year Name}
+#We then 
+
+#We then start a gui asking whether we 
